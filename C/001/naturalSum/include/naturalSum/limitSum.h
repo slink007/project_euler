2791f6bdf3f35451784474dd3513a3e2f4cb75a4
slink007 <stephen.link0@gmail.com>
2020-08-12 20:34:25 -0400

    put include guards on limitSum.h

--- C/001/naturalSum/include/naturalSum/limitSum.h
+++ C/001/naturalSum/include/naturalSum/limitSum.h
@@ -1,5 +1,12 @@
+#ifndef _LIMITSUM_H
+#define _LIMITSUM_H
+
+
 /*
 	Return the sum of all natural numbers which are multiples of n1 or n2
 	and which are less than limit.
 */
 unsigned int limitSum(unsigned int n1, unsigned int n2, unsigned int limit);
+
+
+#endif
