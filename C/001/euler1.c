69d5f670eef7e87b2aa65053e5c361398a2f1d6e
slink007 <stephen.link0@gmail.com>
2020-08-12 20:55:19 -0400

    added ability to pass options to executable

--- C/001/euler1.c
+++ C/001/euler1.c
@@ -6,17 +6,44 @@
  * Answer = 233168
  */
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "naturalSum/limitSum.h"
 
-int main()
+int main(int argc, char ** argv)
 {
-	unsigned int num1 = 3, num2 = 5, limit = 1000;
+	unsigned int num1, num2, limit;
+
+	if ( argc == 2 )
+	{
+		if (strcmp("-h", argv[1]) == 0)
+		{
+			printf("Help file triggered\n");
+			return EXIT_SUCCESS;
+		}
+	}
+	else if (argc == 4)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		limit = atoi(argv[3]);
+	}
+	else if (argc == 1)
+	{
+		num1 = 3;
+		num2 = 5;
+		limit = 1000;
+	}
+	else 
+	{
+		fprintf (stderr,"Usage: %s <int 1> <int 2> <limit>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
 
 	printf("The sum of all the multiples of %d and %d below %d is %d\n", \
 		num1, num2, limit, limitSum(num1, num2, limit));
 
-	return 0;
+	return EXIT_SUCCESS;
 }
