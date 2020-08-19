f217533c00b854b1b0fab0a5ceabeb30a903f4e1
RonObvious007 <iamronobvious@gmail.com>
2020-08-18 21:14:12 -0400

    Changes to be committed: 	modified:   004/numPalindrome/src/palindrome.c
    First attempt at testing to see if a number is a palindrome or not.

--- C/004/numPalindrome/src/palindrome.c
+++ C/004/numPalindrome/src/palindrome.c
@@ -3,14 +3,45 @@
 
 bool isPalindrome(uint32_t number)
 {
-	int32_t length;
+	uint32_t length;
 
 	// how many digits in the number
 	length = snprintf(NULL, 0, "%u", number);
 
-	//debug
-	printf("%u contains %d digits\n", number, length);
-	//debug
+	if (length == 1)
+		return true;
+
+	char * p = NULL;
+	p = malloc( (length + 1) * sizeof(char) );
+
+	if(p)
+	{
+		snprintf(p, length + 1, "%u", number);
+//		uint32_t half = length / 2;
+		boot result = true;
+		char * start = p;
+		char * end = p + (lengh - 1);
+
+		while (end > start)
+		{
+			if ( *(start) != *(end) )
+			{
+				result = false;
+				break;
+			}
+			else
+			{
+				start++;
+				end--
+			}
+		}
+	}
+	else
+	{
+		fprintf(stderr, "Not enough memory\n");
+		exit (EXIT_FAILURE);
+	}
 	
-	return true;
+	free(p);
+	return result;
 }
