4f0db25364c97bca58384306808f76c4da80c8a7
slink007 <stephen.link0@gmail.com>
2020-08-25 16:48:45 -0400

    Changes to be committed: 	new file:   .gitignore 	new file:   CMakeLists.txt 	new file:   arrayFunctions/CMakeLists.txt 	new file:   arrayFunctions/include/arrayFunctions/arrays.h 	new file:   arrayFunctions/src/arrays.c 	new file:   euler8.c 	modified:   ../README

---
+++ C/008/arrayFunctions/include/arrayFunctions/arrays.h
@@ -0,0 +1,15 @@
+#ifndef __ARRAYS_H__
+#define __ARRAYS_H__
+
+#include <stdlib.h>
+#include <stdint.h>
+
+
+/* Iterate through an array which is 'length' items long and looks for
+ * 'n_adjacent' numbers.  Returns the largest product available from
+ * those adjacent numbers.
+ */
+size_t adjProduct(uint32_t n_adjacent, uint8_t char* array, uint32_t length);
+
+
+#endif
