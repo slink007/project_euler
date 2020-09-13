7ae080351369b3ccc7871742de86d1d716cfafad
RonObvious007 <iamronobvious@gmail.com>
2020-09-12 21:18:21 -0400

    Changes to be committed: 	modified:   C/016/CMakeLists.txt 	modified:   C/016/euler16.c 	deleted:    C/016/stringmath/CMakeLists.txt 	deleted:    C/016/stringmath/include/stringmath/add.h 	deleted:    C/016/stringmath/include/stringmath/multiply.h 	deleted:    C/016/stringmath/src/add.c 	deleted:    C/016/stringmath/src/multiply.c
    Solved euler16

--- C/016/euler16.c
+++ C/016/euler16.c
@@ -10,81 +10,116 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "stringmath/multiply.h"
+#include <math.h>    // for log10()
+#include <unistd.h>  // for handling options
 
 
-#define BASE 21
-#define POWER 5
+#define BASE 2
+#define POWER 1000
 
 
 void printError(char* name);
 void printHelp();
-void zeroes(char *c, unsigned int length);
-int numDigits(int n);
 
 
-int main(int argc, char ** argv)
+int main(int argc, char *argv[])
 {
-	// Create an initial product initialized to 1
-	char *product = malloc(2 * sizeof(char));
-	if (product == NULL)
+	
+	unsigned int base = BASE;
+	unsigned int power = POWER;
+	int temp, temp2;
+	
+	// process options from command line
+	int option;
+	while ( (option = getopt(argc, argv, "b:p:h")) != -1)
 	{
-		perror("Unable to allocate memory for the answer\n");
-		exit (EXIT_FAILURE);
+		switch (option)
+		{
+			case 'b':  // choose different base number
+				temp = atoi(optarg);
+				if (temp < 1)
+					printError(argv[0]);
+				else
+					base = (unsigned int)temp;
+				break;
+			case 'p':  // choose different power to raise base to
+				temp2 = atoi(optarg);
+				if (temp2 < 0)
+					printError(argv[0]);
+				else
+					power = (unsigned int)temp2;
+				break;
+			case 'h':  // prints help text
+				printHelp();
+				break;
+			case '?':  // unrecognized options and missing parameters
+				printError(argv[0]);
+				break;
+		}
 	}
-	strcpy(product, "1");
 	
-	// Convert the base number into a string
-	unsigned int baseNum = BASE;
-	char *base = malloc( (numDigits(baseNum) + 1) * sizeof(char) );
-	if (base == NULL)
+	unsigned int limit = 0;  // controls how far to work into digits
+	
+	/* Determine how big the storage needs to be to hold the digits and
+	 * set aside that storage.
+	 * From an online source I found that 
+	 * num. digits = 1 + log (base^power).
+	 * Using some algebra I rearranged this to 
+	 * num.digits = 1 + (power * log(base)).
+	 */
+	unsigned int numDigits = (unsigned int)( 1 + (power * log10( (double)base )) );
+	char *digits = malloc(numDigits * sizeof(char));
+	if (digits == NULL)
 	{
-		perror("Unable to allocate memory to hold the base number\n");
-		exit (EXIT_FAILURE);
+		perror("Not enough memory available\n");
+		return EXIT_FAILURE;
 	}
-	sprintf(base, "%d", baseNum);
+	digits[0] = 1;  // 1 because 1 * anything = that thing
 	
-	// Convert the power into a string
-	unsigned int powerNum = POWER;
-	char *power = malloc( (numDigits(powerNum) + 1) * sizeof(char) );
-	if (base == NULL)
+	
+	/* The 'digits' array is like a scratchpad which will hold the
+	 * product of multiplying by 'base'.  It's reverse order vs. normal
+	 * so MSD -> LSD.  This doesn't matter for this problem as all we
+	 * want is the sum of the digits.
+	 * 
+	 * This works just like old school pencil & paper multiplication.
+	 * Multiply two numbers, if product is more than ten we keep the
+	 * "one's" part of the number and assign the rest to the "carry".
+	 * For the next digit we repeat and add on the "carry" (if any).
+	 */
+	for (unsigned int i = 1; i <= power; i++)
 	{
-		perror("Unable to allocate memory to hold the power number\n");
-		exit (EXIT_FAILURE);
+		unsigned int carry = 0;
+		unsigned int product = 1;
+		for (unsigned int j = 0;  j <= limit; j++)
+		{
+			product = (digits[j] * base) + carry;
+			digits[j] = product % 10;
+			carry = product / 10;
+			
+			if ( (j == limit) && (carry != 0) )
+				limit++;
+		}
 	}
-	sprintf(power, "%d", powerNum);
 	
-	
-	printf("Testing with %s raised to the power of %s\n", base, power);
-	printf("Product initially contains %s\n", product);
-	
-	// Raise the base to the power
-	getProduct(base, power, product);
-	
-	printf("Now product contains %s\n", product);
-	
-	free(product);
-	free(base);
-	free(power);
-	return EXIT_SUCCESS;
-}
-
-
-int numDigits(int n)
-{
-	if (n == 0)
-		return 0;
+	unsigned long long sum = 0;
+	for (unsigned int x = 0; x <= limit; x++)
+		sum += digits[x];
 		
-	return (1 + numDigits(n / 10));
+	printf("\nThe sum of the digits %d raised to the power of %d is %llu\n\n",
+		base, power, sum);
+	
+	free(digits);
+	return EXIT_SUCCESS;
 }
 
 
 void printError(char* name)
 {
-	fprintf (stderr,"Usage: %s\n", name);
+	fprintf (stderr,"\nUsage: %s\n", name);
 	fprintf (stderr,"Usage: %s -h \n", name);
-	fprintf (stderr,"Usage: %s <base> <power>\n", name);
-	fprintf (stderr,"<base> and <power> are both positive integers\n");
+	fprintf (stderr,"Usage: %s -b <base> -p <power>\n", name);
+	fprintf (stderr,"<base> and <power> are both positive integers\n\n");
 	exit (EXIT_FAILURE);
 }
 
@@ -92,22 +127,13 @@ void printError(char* name)
 void printHelp()
 {
     printf("\nProject Euler Program 16\n\n"
-        "21^5 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.\n\n"
+        "2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.\n"
+        "What is the sum of the digits of 2^1000?\n\n"
         "You can solve this problem as-is or optionally enter a different base\n"
-        "number and power to solve variations of this problem.\n\n");
+        "number and power to solve variations of this problem.\n"
+        "Use -b <base> to change the base.\n"
+        "Use -p <power> to change the power.\n\n");
 
     exit (EXIT_SUCCESS);
 }
 
-
-void zeroes(char *c, unsigned int length)
-{
-		char len = (char)length - 1;
-		char i = 0;
-		while (i < len)
-		{
-			*(c + i) = '0';
-			i++;
-		}
-		*(c + i) = '\0';
-}
--- C/016/stringmath/include/stringmath/multiply.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MULTIPLY_H__
-#define __MULTIPLY_H__
-
-#include <stdlib.h>
-#include <string.h>
-
-void getProduct(char *base, char *power, char *product);
-
-#endif
