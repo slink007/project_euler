447e227622a5ab9eee55b43eca99502ade6b5730
slink007 <stephen.link0@gmail.com>
2020-09-22 16:59:18 -0400

    Changes to be committed: 	modified:   022/euler22.c 	modified:   022/names.txt

--- C/022/euler22.c
+++ C/022/euler22.c
@@ -34,6 +34,10 @@ struct _nl
 };
 typedef struct _nl nameList;
 
+void sortNames(nameList *nl);
+void freeNames(nameList *nl);
+size_t charScore(char c);
+size_t score(nameList *nl);
 
 int main(int argc, char *argv[])
 {
@@ -64,81 +68,53 @@ int main(int argc, char *argv[])
 	char *lineptr = NULL;
 	size_t len, read;
 	nameList names;
-	names.list = (char **)malloc(9 * sizeof(char *));
+	names.list = (char **)malloc(sizeof(char *));
 	names.index = -1;
 	names.count = 0;
 	
-	// fill array (works)
-	while (names.index < 9)
+	// fill and sort array
+	/* TODO
+	 * Using getdelim only works if I modify the supplied file and add a final comma onto 
+	 * the end of it.  Rework this section to use getc so that ALL the characters can be
+	 * read and used without modifying the file.
+	 */
+	while ( (read = getdelim(&lineptr, &len, 44, f)) != EOF)  // 44 is a comma
 	{
-		read = getdelim(&lineptr, &len, 44, f);  // 44 is a comma
-		if (read > 0)  // we got a name
-		{
-			// make index correct for where in the list to make storage for
-			// the new name
-			names.index += 1;
-			
-			// count the new name
-			names.count += 1;
-			
-			// add storage for the new name to the list
-			// +1 is for terminating '\0'
-			*((names.list) + names.index) = malloc( (read + 1) * sizeof(char));
-			
-			// copy the name we read into the storage
-			strcpy(*((names.list) + names.index), lineptr);
-			
-			// grow the list to allow for another line in the future
-			names.list = (char **)realloc(names.list, (names.count + 1) * sizeof(char *));
-			
-			// debug, verification that we are writing to list correctly
-			printf("%s is at index %d\n", *((names.list) + names.index), names.index );
-		}
+		// make index correct for where in the list to make storage for
+		// the new name
+		names.index += 1;
+		
+		// count the new name
+		names.count += 1;
+		
+		// add storage for the new name to the list
+		// +1 is for terminating '\0'
+		*((names.list) + names.index) = malloc( (read + 1) * sizeof(char));
+		
+		// copy the name we read into the storage
+		strcpy(*((names.list) + names.index), lineptr);
+		
+		// sort the list as names are added
+		sortNames(&names);
+		
+		// grow the list to allow for another line in the future
+		names.list = (char **)realloc(names.list, (names.count + 1) * sizeof(char *));
 	}
 	
-	if ( strcmp( *((names.list) + 1), *((names.list) + 2) ) > 0 )
-	{
-		char *temp = *((names.list) + 1);
-		*((names.list) + 1) = *((names.list) + 2);
-		*((names.list) + 2) = temp;
-	}
-
-	for (int i = 0; i < names.count; i++)
+	/*for (int i = 0; i < names.count; i++)
 		printf("%s\n", *((names.list) + i));
+	*/
+	
+	// test score
+	printf("\n%zu\n", score(&names));
 	
 	fclose(f);
 	free(lineptr);
-	
-	
-	while (names.index >= 0)
-	{
-		free( *((names.list) + names.index) );
-		names.index -= 1;
-	}
-	free(names.list);
-	
+	freeNames(&names);
+
 	return EXIT_SUCCESS;
 }
 
-
-/* Function to sort an array using insertion sort*/
-void insertionSort(int arr[], int n) 
-{ 
-    int i, key, j; 
-    for (i = 1; i < n; i++) { 
-        key = arr[i]; 
-        j = i - 1; 
-  
-        /* Move elements of arr[0..i-1], that are 
-          greater than key, to one position ahead 
-          of their current position */
-        while (j >= 0 && arr[j] > key) { 
-            arr[j + 1] = arr[j]; 
-            j = j - 1; 
-        } 
-        arr[j + 1] = key; 
-    }
-} 
 
 void sortNames(nameList *nl)
 {
@@ -146,9 +122,11 @@ void sortNames(nameList *nl)
 	{
 		for (int j = i - 1; j >= 0; j--)
 		{
+			/* If the name below the current name is larger than the current name
+			 * do an insertion swap.  If not then no need to keep trying.
+			 */
 			if ( strcmp( *((nl->list) + j), *((nl->list) + j + 1) ) > 0 )
 			{
-				// swap
 				char *temp = *((nl->list) + j);
 				*((nl->list) + j) = *((nl->list) + j + 1);
 				*((nl->list) + j + 1) = temp;
@@ -157,4 +135,53 @@ void sortNames(nameList *nl)
 				break;
 		}
 	}
+}
+
+
+void freeNames(nameList *nl)
+{
+	while (nl->index >= 0)
+	{
+		free( *((nl->list) + nl->index) );
+		nl->index -= 1;
+	}
+	free(nl->list);
+}
+
+
+size_t charScore(char c)
+{
+	switch (c)
+	{
+		case '"':
+		case ',':
+			return 0;
+			break;
+		default:
+			// Correct values for 'A' to 'Z' but will break on other values.
+			return (size_t)( (c - 'A') + 1);
+			break;
+	}
+}
+
+
+size_t score(nameList *nl)
+{
+	size_t total = 0;
+	//int foo = 0;
+	while ( (nl->index) > 0)
+	{
+		size_t subtotal = 0;
+		int length = (int)strlen(*((nl->list) + nl->index));
+		for (int i = 0; i < length; i++)
+			subtotal += charScore( *(*((nl->list) + nl->index) + i) );
+		printf("%s ==> %zu * %d = ", *((nl->list) + nl->index), subtotal, (nl->index));
+		subtotal *= (nl->index);
+		printf("%zu\n", subtotal);
+		nl->index -= 1;
+		//foo += 1;
+		total += subtotal;
+	}
+	
+	return total;
 }
