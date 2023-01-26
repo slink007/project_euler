41db104e3521024086c482c01afd0df5cf77501a
slink007 <stephen.link0@gmail.com>
2023-01-25 22:03:44 -0500

    Simplify euler1.py

--- Python/001/euler1.py
+++ Python/001/euler1.py
@@ -11,17 +11,9 @@ class Euler1:
             except (TypeError, AssertionError):
                 raise ValueError("Invalid limit value")
 	
-    def __repr__(self):
-        p1 = "Project Euler Problem 1:\n\n"
-        p2 = "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and" +\
-        "9.  The sum of these multiples is 23.\n\n"
-        p3 = "Find the sum of all the multiples of 3 or 5 below 1000."
-        return p1 + p2 + p3
-
     def solve(self):
         multiples = [n for n in range(3, self.limit) if ( (n % 3 == 0) or (n % 5 == 0) )]
         return sum(multiples)
-
 
 
 if __name__ == '__main__':
@@ -30,11 +22,8 @@ if __name__ == '__main__':
     
     limit = 1000
         
-    opts, args = getopt.getopt(sys.argv[1:], "hl:")
+    opts, args = getopt.getopt(sys.argv[1:], "l:")
     for opt, arg in opts:
-        if opt in ['-h']:         # tell the user what the program does
-            print(Euler1())
-            exit()
         if opt in ['-l']:         # set a different limit
             limit = arg
 
